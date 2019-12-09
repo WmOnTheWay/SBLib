2bed629d0d3f7d2388f62642812d5852204ba856
WmOnTheWay <1293822641@qq.com>
2019-12-09 11:43:24 +0800

    Signed-off-by: WmOnTheWay <1293822641@qq.com>

---
+++ js/CAB.js
@@ -0,0 +1,41 @@
+import { Context } from "mocha";
+
+Function.prototyoe.myCall = function(context){
+    context = context||window;
+    context.fn = this;
+    const args = [...arguments].slice(1);
+    results = context.fn(...args);
+    delete context.fn;
+    return results;
+}
+
+Function.prototype.myApply = function(context){
+    if(typeof this !== 'function'){
+        throw new TypeError('Error');
+    }
+    context = context||window;
+    context.fn = this;
+    let result;
+    if(arguments[1]){
+        result = context.fn(...arguments[1]);
+    }else{
+        result = context.fn()
+    }
+    delete context.fn;
+    return result;
+}
+
+Function.prototype.myBind = function(context){
+    if (typeof this !== 'function'){
+        throw new TypeError('Error');
+    }
+    const _this = this;
+    const args = [...arguments].slice(1);
+
+    return function fn(){
+        if (this instanceof fn){
+            return new _this(...args,...arguments);
+        }
+        return _this.apply(context,args.concat(...arguments));
+    }
+}
