f63b46b7856bca8bf83fd5e6429d41eea12bc892
WmOnTheWay <1293822641@qq.com>
2020-01-03 14:44:13 +0800

    Signed-off-by: WmOnTheWay <1293822641@qq.com>

---
+++ js/debounce&throttle.js
@@ -0,0 +1,28 @@
+function debounce(fn, delay, immediate) {
+    let timer = null;
+    return function () {
+        let context =  this;
+        let args = arguments;
+        if(timer&&immediate){
+            fn.apply(context, args);
+        }
+        if (timer) { clearTimeout(timer); }
+        timer = setTimeout(()=>{fn.apply(context,args)}, delay);
+    }
+}
+
+function throttle(fn, delay,immediate) {
+    let callNow = immediate,
+        timer=null;
+    return function () {
+        let context =this;
+        let args = arguments;
+        if(callNow){fn.apply(context,args);callNow=false}
+        if(!timer){
+            timer=setTimeout(()=>{
+                fn.apply(context, args);
+                timer=null;
+            }, delay);
+        }
+    }
+}
