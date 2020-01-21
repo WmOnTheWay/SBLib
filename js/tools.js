c9225f06c0456310281bb0b72f5072b5ff696e25
WmOnTheWay <1293822641@qq.com>
2020-01-21 18:24:13 +0800

    add assiGn

--- js/tools.js
+++ js/tools.js
@@ -58,4 +58,32 @@ function create(){
 }
 
 
+//模拟assign
+if(typeof Object.assiGn !='function'){
+	Object.defineProperty(Object,'assiGn',{
+		value:function(target){
+			'use strict';
+			if(target==null){
+				throw new TypeError('cannot convert defined or null to object!');
+			}
+			var to = Object(target);
+			for(var i = 1;i< argumnets.length;i++){
+				var nextSource = arguments[i];
+				if(nextSource != null){
+					for(var nextKey in nextSource){
+						if(Object.prototype.hasOwnProperty.call(nextSource,nextKey)){
+							to[nextKey]=nextSource[nextKey];
+						}
+					}
+				}
+			}
+			return to;
+		},
+		enumerable:false,
+		writable:true,
+		configurable:true
+	});
+}
+
+
 export default tools;
